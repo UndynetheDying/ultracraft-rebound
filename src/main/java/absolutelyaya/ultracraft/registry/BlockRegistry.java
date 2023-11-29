9935e75835857e0577a378e237e0356294717990
yaya-nyan <70072204+absolutelyaya@users.noreply.github.com>
2023-11-29 05:23:43 +0100

    skyblocks no longer allow mobs to spawn on them naturally

--- src/main/java/absolutelyaya/ultracraft/registry/BlockRegistry.java
+++ src/main/java/absolutelyaya/ultracraft/registry/BlockRegistry.java
@@ -70,7 +70,8 @@ public class BlockRegistry
 	public static final Block HELL_SPAWNER = register("hell_spawner",
 			new HellSpawnerBlock(AbstractBlock.Settings.copy(Blocks.NETHERRACK).sounds(BlockSoundGroup.MUD).mapColor(MapColor.DARK_CRIMSON).nonOpaque()), false);
 	public static final Block SKY_BLOCK = register("sky_block",
-			new SkyBlock(AbstractBlock.Settings.copy(Blocks.BEDROCK).sounds(BlockSoundGroup.GLASS).mapColor(MapColor.BLACK).pistonBehavior(PistonBehavior.BLOCK)), false);
+			new SkyBlock(AbstractBlock.Settings.copy(Blocks.BEDROCK).sounds(BlockSoundGroup.GLASS).mapColor(MapColor.BLACK).pistonBehavior(PistonBehavior.BLOCK)
+								 .allowsSpawning((state, blockView, blockPos, entityType) -> false)), false);
 	public static final Block SLAB_BLOCK = register("slab_block",
 			new SlabBlock(AbstractBlock.Settings.copy(Blocks.SMOOTH_STONE).mapColor(MapColor.LIGHT_GRAY).luminance(b -> b.get(RedstoneLampBlock.LIT) ? 5 : 0)), true);
 	
