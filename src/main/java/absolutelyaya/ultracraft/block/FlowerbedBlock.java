289a37b49498b05812fb042b73cbb240533a1dbf
yaya-nyan <70072204+absolutelyaya@users.noreply.github.com>
2023-12-23 14:20:40 +0100

    Added Hanging side bits to Flowerbeds

--- src/main/java/absolutelyaya/ultracraft/block/FlowerbedBlock.java
+++ src/main/java/absolutelyaya/ultracraft/block/FlowerbedBlock.java
@@ -1,16 +1,15 @@
 package absolutelyaya.ultracraft.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.EntityShapeContext;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
@@ -19,16 +18,22 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 public class FlowerbedBlock extends Block
 {
 	public static final IntProperty LAYERS = IntProperty.of("layers", 1, 4);
+	public static final BooleanProperty NORTH = Properties.NORTH;
+	public static final BooleanProperty EAST = Properties.EAST;
+	public static final BooleanProperty SOUTH = Properties.SOUTH;
+	public static final BooleanProperty WEST = Properties.WEST;
 	
 	public FlowerbedBlock(Settings settings)
 	{
 		super(settings);
-		setDefaultState(stateManager.getDefaultState().with(LAYERS, 1));
+		setDefaultState(stateManager.getDefaultState().with(LAYERS, 1)
+								.with(NORTH, false).with(EAST, false).with(SOUTH, false).with(WEST, false));
 	}
 	
 	@Override
@@ -64,6 +69,21 @@ public class FlowerbedBlock extends Block
 	}
 	
 	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos)
+	{
+		return super.getStateForNeighborUpdate(getSideState(world, pos, state), direction, neighborState, world, pos, neighborPos);
+	}
+	
+	BlockState getSideState(WorldAccess world, BlockPos pos, BlockState state)
+	{
+		if(world.getBlockState(pos.down()).isSideSolid(world, pos, Direction.UP, SideShapeType.FULL))
+			return state.with(NORTH, world.isAir(pos.north().down())).with(EAST, world.isAir(pos.east().down()))
+					.with(SOUTH, world.isAir(pos.south().down())).with(WEST, world.isAir(pos.west().down()));
+		else
+			return state;
+	}
+	
+	@Override
 	public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
 		return 1f;
 	}
@@ -80,7 +100,8 @@ public class FlowerbedBlock extends Block
 	}
 	
 	@Override
-	public boolean canReplace(BlockState state, ItemPlacementContext context) {
+	public boolean canReplace(BlockState state, ItemPlacementContext context)
+	{
 		int layers = state.get(LAYERS);
 		if (context.getStack().isOf(asItem()) && layers < 4)
 		{
@@ -93,15 +114,20 @@ public class FlowerbedBlock extends Block
 	
 	@Override
 	@Nullable
-	public BlockState getPlacementState(ItemPlacementContext ctx) {
+	public BlockState getPlacementState(ItemPlacementContext ctx)
+	{
 		BlockState state = ctx.getWorld().getBlockState(ctx.getBlockPos());
 		if (state.isOf(this))
 			return state.with(LAYERS, Math.min(4, state.get(LAYERS) + 1));
-		return super.getPlacementState(ctx);
+		else
+			state = getDefaultState();
+		state = getSideState(ctx.getWorld(), ctx.getBlockPos(), state);
+		return state;
 	}
 	
 	@Override
-	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(LAYERS);
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder)
+	{
+		builder.add(LAYERS, NORTH, EAST, SOUTH, WEST);
 	}
 }
