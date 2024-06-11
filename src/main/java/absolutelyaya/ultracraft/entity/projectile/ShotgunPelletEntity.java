4f97d34cabfd78610b543d28e61e1948a9752d30
yaya <70072204+absolutelyaya@users.noreply.github.com>
2024-06-11 23:03:05 +0200

    possibly fixed the ever-returning self-shotgunning issue again

--- src/main/java/absolutelyaya/ultracraft/entity/projectile/ShotgunPelletEntity.java
+++ src/main/java/absolutelyaya/ultracraft/entity/projectile/ShotgunPelletEntity.java
@@ -18,7 +18,6 @@ import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
@@ -98,7 +97,7 @@ public class ShotgunPelletEntity extends HellBulletEntity implements ProjectileE
 		if(!entity.getClass().equals(ignore) && !isOwner(entity))
 			entity.damage(DamageSources.get(getWorld(), DamageSources.SHOTGUN, getOwner()),
 					damage * ServerConfig.INSTANCE.shotgunDamage.getValue());
-		if(parried)
+		if(parried && !entity.equals(((ProjectileEntityAccessor)this).getParrier()))
 			onParriedCollision(entityHitResult);
 	}
 	
