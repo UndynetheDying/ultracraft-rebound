566498983e39ad9ebce4e639a90f4d4200ecc28b
yaya <70072204+absolutelyaya@users.noreply.github.com>
2024-06-24 08:47:07 +0200

    removed "Glitched" Blockstate from fake leaves

--- src/main/java/absolutelyaya/ultracraft/block/FakeLeavesBlock.java
+++ src/main/java/absolutelyaya/ultracraft/block/FakeLeavesBlock.java
@@ -3,8 +3,6 @@ package absolutelyaya.ultracraft.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -12,19 +10,9 @@ import net.minecraft.world.BlockView;
 
 public class FakeLeavesBlock extends Block
 {
-	public static BooleanProperty GLITCH = BooleanProperty.of("glitch");
-	
 	public FakeLeavesBlock(Settings settings)
 	{
 		super(settings);
-		setDefaultState(getDefaultState().with(GLITCH, false));
-	}
-	
-	@Override
-	protected void appendProperties(StateManager.Builder<Block, BlockState> builder)
-	{
-		super.appendProperties(builder);
-		builder.add(GLITCH);
 	}
 	
 	@Override
